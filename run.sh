9d6f217ad3a5ca26070aaf34db3d6fe63851898d
root <root@elegant-light-1.localdomain>
2021-01-16 18:49:37 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+cd `dirname $0`
 ps -ef | grep 'twitter_following_monitor' | grep -v 'grep' | cut -c 9-16 | xargs kill -9
 # setsid python3 ./twitter_following_monitor.py run --username ionicbond3 --log_path ~/ion_monitor.log &
 setsid python3 ./twitter_following_monitor.py run --username minatoaqua --log_path ~/aqua_monitor.log &
