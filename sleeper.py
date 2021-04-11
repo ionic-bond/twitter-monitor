42ad1ba089bcd1507c5346b34ac7b1497a72b633
ionic_bond <ionicbond3@gmail.com>
2021-04-11 23:30:12 +0800

    fix bug

--- sleeper.py
+++ sleeper.py
@@ -6,6 +6,7 @@ import time
 class Sleeper:
 
     def __init__(self, original_sleep_second):
+        self.original_sleep_second = original_sleep_second
         self.sleep_second = original_sleep_second
         self.normal_count = 0
 
@@ -13,7 +14,7 @@ class Sleeper:
     def sleep(self, normal: bool):
         if normal:
             self.normal_count += 1
-            if self.normal_count > 20 and self.sleep_second > MIN_SLEEP_SECOND:
+            if self.normal_count > 20 and self.sleep_second > self.original_sleep_second:
                 self.sleep_second /= 2
                 logging.info('Changed sleep second to {}'.format(self.sleep_second))
         else:
