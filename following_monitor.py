421a7cf2d618eabc53b46b97d4c5f35413f6fc08
ionic-bond <ionicbond3@gmail.com>
2021-04-17 12:58:04 +0000

    optimize

--- following_monitor.py
+++ following_monitor.py
@@ -46,12 +46,12 @@ class Monitor:
         max_changes = max(len(old_following_users) / 2, 10)
         if abs(len(old_following_users) - len(new_following_users)) > max_changes:
             return
-        inc_users = new_following_users - old_following_users
-        if inc_users:
-            self.telegram_notifier.send_message('Follow: {}'.format(inc_users))
         dec_users = old_following_users - new_following_users
         if dec_users:
             self.telegram_notifier.send_message('Unfollow: {}'.format(dec_users))
+        inc_users = new_following_users - old_following_users
+        if inc_users:
+            self.telegram_notifier.send_message('Follow: {}'.format(inc_users))
 
 
     def run(self):
--- sleeper.py
+++ sleeper.py
@@ -14,11 +14,12 @@ class Sleeper:
     def sleep(self, normal: bool):
         if normal:
             self.normal_count += 1
-            if self.normal_count > 20 and self.sleep_second > self.original_sleep_second:
+            if self.normal_count > 3 and self.sleep_second > self.original_sleep_second:
                 self.sleep_second /= 2
                 logging.info('Changed sleep second to {}'.format(self.sleep_second))
         else:
             self.normal_count = 0
-            self.sleep_second *= 2
-            logging.info('Changed sleep second to {}'.format(self.sleep_second))
+            if self.sleep_second < 600:
+                self.sleep_second *= 2
+                logging.info('Changed sleep second to {}'.format(self.sleep_second))
         time.sleep(self.sleep_second)
