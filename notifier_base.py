8cacfcf3b2a672378fc469f268aef0114184f93c
ionic-bond <ionicbond3@gmail.com>
2022-10-05 03:36:36 +0800

    add notifier base class

---
+++ notifier_base.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import queue
+import threading
+from abc import ABC, abstractmethod
+from typing import List, Union
+
+class Message:
+
+    def __init__(self,
+                 text: str,
+                 photo_url_list: Union[List[str], None] = None,
+                 video_url_list: Union[List[str], None] = None):
+        self.text = text
+        self.photo_url_list = photo_url_list
+        self.video_url_list = video_url_list
+
+
+class NotifierBase(ABC):
+    message_queue = queue.SimpleQueue()
+    initialized = False
+
+    def __new__(self):
+        raise Exception('Do not instantiate this class!')
+
+    @classmethod
+    @abstractmethod
+    def init(cls):
+        cls.initialized = True
+        cls.work_start()
+    
+    @classmethod
+    @abstractmethod
+    def send_message(cls, message: Message):
+        pass
+    
+    @classmethod
+    def _work(cls):
+        while True:
+            message = cls.message_queue.get()
+            cls.send_message(message)
+
+    @classmethod
+    def work_start(cls):
+        threading.Thread(target=cls._work, daemon=True).start()
+
+    @classmethod
+    def put_message(cls, message: Message):
+        cls.message_queue.put(message)
